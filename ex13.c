6b16093ca30fa27d089539af61b11f7c18eaa335
Filippo Chiarion <153537495+FilippoChiarion@users.noreply.github.com>
2024-09-20 18:34:03 -0300

    ,

--- ex13.c
+++ ex13.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 void main(){
-    int vet[10],i,j=0,repetido;//mudar vet[10] para [50]
+    int vet[50],i,j=0,repetido;//mudar vet[10] para [50]
 
-    for(i=0;i<10;i++){
+    for(i=0;i<50;i++){
         printf("Numero %i: ",i+1);
         scanf("%i",&vet[i]);
     }
 
-    for(i=0;i<10;i++){
+    for(i=0;i<50;i++){
             repetido=0;
         for(j=0;j<i;j++){
             if(vet[i]==vet[j]){
